5a687a23322814e339675ad7bf84d97dcdce89f1
Marco DLC <140775824+Marco-DLC@users.noreply.github.com>
2024-02-19 16:56:58 -0500

    Add drawing feature using mousedown event listener

--- script.js
+++ script.js
@@ -9,10 +9,18 @@ function createGrid(num) {
     for (let i = 0; i < num * num; i++) {
         const square = document.createElement('div');
         square.classList.add('square');
+
         square.style.width = squareWidth;
         square.style.paddingTop = squareWidth;
+
+        square.addEventListener('mouseout', () => {
+            square.style.backgroundColor = 'rgb(44, 53, 43)';
+        });
+
         container.appendChild(square);
     };
 };
 
 createGrid(desiredGridWidth);
+
+
