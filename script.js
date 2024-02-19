790f999a2585d46eaf4e637ee10d354d8e89ffdf
Marco DLC <140775824+Marco-DLC@users.noreply.github.com>
2024-02-19 15:34:24 -0500

    Add function that creates flexible grid
    This commit adds a versatile 'createGrid' function, designed to
    generate a square grid using the 'desiredGridWidth' variable. The
    function  appends the required number of square divs to form a square
    grid.
    
    I removed the borders of each square div to give the grid a more
    unified appearance, transforming the grid into a cohesive 'screen-like'
    interface, rather than a conventional grid structure. An inner
    container serves as a border around the entire grid for style.

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-const square = document.createElement('div');
-const container = document.querySelector('#container');
+const container = document.querySelector('#innerContainer');
+const desiredGridWidth = 16;
 
-square.classList.add('square');
-container.appendChild(square);
+function createGrid(num) {
+    container.innerHtml = '';
+
+    const squareWidth = `${100 / num}%`;
+
+    for (let i = 0; i < num * num; i++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        square.style.width = squareWidth;
+        square.style.paddingTop = squareWidth;
+        container.appendChild(square);
+    };
+};
+
+createGrid(desiredGridWidth);
